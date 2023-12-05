f5a4c0eda7b61ce56e4ccea249024323dc99d5fc
Maxim <Zholdashev.Maks@yandex.ru>
2023-12-05 21:14:01 +0300

    Adding Roles

--- register.php
+++ register.php
@@ -1,41 +1,17 @@
-<?php  
-// подключение к базе данных
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "training";
+<?php
+session_start();
+include("./includes/connect.php");
+include("./functions/process_reg.php");
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-// проверка подключения
-if ($conn->connect_error) {
-    die("connect failed: " . $conn->connect_error);
-}
-// обработка данных из формы регистрации
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
-
-    // ВСТАВКА данных в таблицу пользователей
-    $query = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
-
-    if ($conn->query($query) === TRUE) {
-        echo "Вы зарегистрированы";
-    } else {
-        echo "Ошибка: " . $conn->error;
-    }
-}
-
-//закрытие соединения с базой данных
 $conn->close();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+    <?php
+        include("./templates/head.php"); 
+    ?>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <title>Регистрация</title>
 </head>
 <body>
